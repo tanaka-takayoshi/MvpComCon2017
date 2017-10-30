20ecda253a1528bfceec13887107ed508c7e4668
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2017-10-30 14:49:41 +0900

    dump.

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ChatApp.Hubs
@@ -10,7 +11,11 @@ namespace ChatApp.Hubs
     {
         public void Send(string message)
         {
-            var name2 = Context.User.FindAll("email").FirstOrDefault()?.Value ?? "anonymous@example.net";
+            var name2 = Context.User.FindAll(c =>
+            {
+                Console.WriteLine(c.Type + "=" + c.Value);
+                return c.Type == "emails";
+                }).FirstOrDefault()?.Value ?? "anonymous@example.net";
             // Call the broadcastMessage method to update clients.
             Clients.All.InvokeAsync("broadcastMessage", name2, message);
         }
